95248edf613fc273baf0a85ed9e27ad36a8b01d5
Ramsay Domloge <ramsay.domloge@bca.com>
2018-07-08 11:02:21 +0100

    Correlation changes

---
+++ src/test/java/com/example/faasgw/CorrelationTest.java
@@ -0,0 +1,82 @@
+package com.example.faasgw;
+
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.faas.dto.JobRequest;
+import com.example.faas.dto.JobResponse;
+import com.example.faasgw.ex.CorrelationTimeoutException;
+import com.example.faasgw.ex.NonCorrelationException;
+
+class CorrelationTest {
+
+	private Correlation target;
+	
+	private CorrelationIdGenerator idgen;
+	
+	private ScheduledExecutorService exec;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		target = new Correlation();
+		idgen = new CorrelationIdGenerator();
+		exec = Executors.newScheduledThreadPool(100);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		exec.shutdownNow();
+	}
+
+	class Consumer implements Runnable {
+		protected String id;
+		public Consumer(String id) {
+			this.id = id;
+		}
+		@Override
+		public void run() {
+			try {
+				target.waitFor(id);
+			} catch (CorrelationTimeoutException | InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	class Producer extends Consumer {
+
+		public Producer(String id) {
+			super(id);
+		}
+		
+		public void run() {
+			String json = "adsf";
+			Map<String, String> params = null;
+			JobRequest request = new JobRequest("test", params, null, id);
+			JobResponse response = new JobResponse(request, json);
+			try {
+				target.responseReceived(id, response);
+			} catch (NonCorrelationException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Test
+	void test() throws InterruptedException {
+		for(int i=0; i < 50; i++) {
+			String id = idgen.createCorrelationId();
+			exec.schedule(new Consumer(id), 100, TimeUnit.MILLISECONDS);
+			exec.schedule(new Producer(id), 103, TimeUnit.MILLISECONDS);
+		}
+		
+		Thread.sleep(15000);
+	}
+
+}
